a5d42d676018ef5f4f0fe7e9ceab67c9009b79eb
chibik777 <uset88358@gmail.com>
2025-09-14 18:51:59 +0300

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,27 +1,3 @@
-local Library = loadstring(HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
- 
-local Window = Library.CreateLib("Universal script", "RJTheme3")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Tab = Window:NewTab("Main")
-
-local Section = Tab:NewSection("Misc")
-
-Section:NewToggle("Noclip", "???" function(state)
-    if state then
-        game.RunService.Stepped:Connect(function() game.Players.LocalPlayer.Character.Head.CanCollide = false game.Players.LocalPlayer.Character.Torso.CanCollide = false end)
-    else
-        game.RunService.Stepped:Connect(function() game.Players.LocalPlayer.Character.Head.CanCollide = true game.Players.LocalPlayer.Character.Torso.CanCollide = true end)
-    end
-end)
-
-Section:NewSlider("WalkSpeed", "Flash", 500, 0, function(s) 
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-end)
-
-
-
-local Section = Tab:NewSection("Soon...")
-
-local Tab = Window:NewTab("Other")
-
-local Section = Tab:NewSection("Soon...")
+local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
